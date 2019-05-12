f5df2948b03ca2abc64dbb2f0706ae6a1912fb4a
Michael Roeslein <michaeljroeslein@gmail.com>
2019-05-12 16:16:10 -0400

    Added button to delete individual movie from Store

--- client/dist/index.js
+++ client/dist/index.js
@@ -25803,20 +25803,49 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _MovieContext = require("./MovieContext.jsx");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var Movie = function Movie(props) {
+  var _useContext = (0, _react.useContext)(_MovieContext.MovieContext),
+      _useContext2 = _slicedToArray(_useContext, 2),
+      movies = _useContext2[0],
+      setMovies = _useContext2[1];
+
   var _props$movie = props.movie,
       name = _props$movie.name,
       price = _props$movie.price;
-  return _react.default.createElement("ul", null, _react.default.createElement("li", null, name), _react.default.createElement("li", null, price));
+
+  var handleClick = function handleClick(e) {
+    var index = movies.map(function (movie) {
+      return movie.name;
+    }).indexOf(name);
+    setMovies(function (prevMovies) {
+      var updatedMovies = Array.from(prevMovies);
+      updatedMovies.splice(index, 1);
+      return updatedMovies;
+    });
+  };
+
+  return _react.default.createElement("ul", null, _react.default.createElement("li", null, name), _react.default.createElement("li", null, price), _react.default.createElement("button", {
+    onClick: handleClick
+  }, "x"));
 };
 
 var _default = Movie;
 exports.default = _default;
-},{"react":"../../node_modules/react/index.js"}],"components/MovieList.jsx":[function(require,module,exports) {
+},{"react":"../../node_modules/react/index.js","./MovieContext.jsx":"components/MovieContext.jsx"}],"components/MovieList.jsx":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "./MovieContext.jsx";
 
 const Movie = props => {
+  const [movies, setMovies] = useContext(MovieContext);
   const { name, price } = props.movie;
+
+  const handleClick = e => {
+    const index = movies.map(movie => movie.name).indexOf(name);
+    setMovies(prevMovies => {
+      const updatedMovies = Array.from(prevMovies);
+      updatedMovies.splice(index, 1);
+      return updatedMovies;
+    });
+  };
+
   return (
     <ul>
       <li>{name}</li>
       <li>{price}</li>
+      <button onClick={handleClick}>x</button>
     </ul>
   );
 };
